494763019b96b9a01516c56a3b543b9d3c776f5c
ac065455 <chen.anthony.1109@gmail.com>
2018-02-27 14:59:06 -0500

    added masonry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './static/css/styles.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // Componenets
 import App from './components/app';
+import ThreeEightSevenFive from './components/threeeightsevenfive';
+import TwoNineFive from './components/twoninefive';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/3875" component={ThreeEightSevenFive} />
+      <Route path="/295" component={TwoNineFive} />
+    </Switch>
+  </BrowserRouter>
+  , document.getElementById('root'));
