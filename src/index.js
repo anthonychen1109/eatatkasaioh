9a53dd774e92de2aa6c0ccaae2371a20afb15dee
ac065455 <chen.anthony.1109@gmail.com>
2018-02-27 16:07:51 -0500

    added props to route

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
-      <Route path="/3875" component={ThreeEightSevenFive} />
-      <Route path="/295" component={TwoNineFive} />
+      <Route path="/3875" render={() => <ThreeEightSevenFive storeNo="1"/>}/>
+      <Route path="/295" render={() => <TwoNineFive storeNo="2"/>}/>
     </Switch>
   </BrowserRouter>
   , document.getElementById('root'));
